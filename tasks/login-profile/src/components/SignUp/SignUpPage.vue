adb0a5be089169473d78f3bd1428b53446573cdd
kirankumar3117 <ch.kirankumar311@gmail.com>
2023-03-21 16:48:29 +0530

    login-profile

---
+++ tasks/login-profile/src/components/SignUp/SignUpPage.vue
@@ -0,0 +1,119 @@
+<template >
+  <div>
+
+    <LoadingSpinner :loading="loading"/>
+
+    <div :id="loading ? 'disable' : ''">
+    <div class="container d-flex flex-row justify-content-center mt-5 ml-auto mr-auto" >
+      
+      <form class="form" @submit="getDetails">
+          <LoginSignUpError text="SignUp" :error="error"/>
+        <div class="mb-3">
+          <label  class="form-label">Email address</label>
+          <input type="email" class="form-control"  aria-describedby="emailHelp" v-model='email' placeholder="name@gmail.com" required="true"/>
+          <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        </div>
+        <div class="mb-3">
+          <label  class="form-label">Password</label>
+          <input type="password" class="form-control" v-model='password' placeholder="enter password here" required=true/>
+        </div>
+        <div class="mb-3">
+          <label  class="form-label">Re-enter Password</label>
+          <input type="password" class="form-control" v-model='repassword' placeholder="re-enter password here" required="true">
+        </div>
+        <input type="submit" class="btn btn-primary"  value="submit">
+      </form>
+    </div>
+    <br/>
+    <br/>
+    <LoginSignUpRoute text='Already a user' path="login" button='Login'/>
+</div>
+
+
+
+</div>
+
+
+</template>
+
+
+<script>
+import LoginSignUpRoute from '../ReusableComponets/LoginSignUpRoute.vue';
+import LoginSignUpError from '../ReusableComponets/LoginSignUError.vue';
+import LoadingSpinner from "../ReusableComponets/LoadingSpinner.vue"
+import router  from '@/router/index.js';
+import axios from 'axios';
+export default {
+
+
+
+    name:'SignUpPage',
+    components: {
+         LoginSignUpRoute,
+         LoginSignUpError,
+         LoadingSpinner
+     },
+    data(){
+        return {
+            email:'',
+            password:'',
+            repassword:'',
+            error:false ,
+            loading:false,
+           
+        }
+        
+    },
+    methods:{
+       async getDetails(e){
+          
+
+        e.preventDefault()
+
+       
+
+        if(this.password != this.repassword){
+            this.error=true;
+            this.password='';
+            this.repassword='';
+            return;
+        }
+
+        this.loading=true;
+        this.error=false;
+
+        try{
+          const res=await axios.post(`${process.env.VUE_APP_API}/register`,{
+            email:this.email,
+            password:this.password
+          })
+          console.log(res.data);
+                   this.loading=false;
+                  router.push({ path: '/login' })
+        }catch(err){
+          console.log({message:err.message});
+                    this.error=true;
+                    this.password='';
+                    this.repassword='';
+                    this.loading=false;
+        }
+
+            }
+    }
+
+    
+}
+
+
+</script>
+
+
+<style >
+
+#disable{
+    pointer-events: none;
+    opacity: 0.4;
+}
+
+
+</style>
---
+++ tasks/login-profile/src/views/LoginView.vue
@@ -0,0 +1,16 @@
+<template>
+    <LoginPage></LoginPage>
+</template>
+
+<script>
+import LoginPage from "@/components/Login/LoginPage.vue";
+import check from "@/mixins/check.js"
+export default{
+    name:'LoginView',
+    components:{
+        LoginPage
+    },
+    mixins:[check]
+}
+
+</script>
