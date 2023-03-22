d714c5ad7d68c6b9578157fa4273c71b8e48f129
kirankumar3117 <ch.kirankumar311@gmail.com>
2023-03-22 11:57:17 +0530

    push

---
+++ tasks/store/src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    {{ count }}
+    <h1>This is home page.</h1>
+  </div>
+</template>
+
+<script >
+// @ is an alias to /src
+import {useCounterStore} from "@/stores/counter";
+
+export default {
+  name: "HomeView",
+  setup(){
+    
+  },
+  data(){
+    return{
+      count:()=>{
+        const {count}=useCounterStore();
+        return count
+      }
+    }
+  },
+ 
+};
+</script>
